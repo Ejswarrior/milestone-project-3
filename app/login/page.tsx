6d514a44d5063975f46fcea2ea22837b15dcf906
Erik Sabin <eriksabin97@gmail.com>
2023-03-22 20:42:04 -0400

    Moved folders

--- app/login/page.tsx
+++
@@ -1,2 +0,0 @@
-import styles from './Login.module.scss'
-
--- utils/connectDB.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const connectDB = async() => {
-    await mongoose.connect("mongodb://localhost:27017/productivity");
-}
-
-
-export default connectDB
